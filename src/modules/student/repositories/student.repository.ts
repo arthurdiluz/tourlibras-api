c3a4503f16eb171f97729f5a539b423d35494bb5
Arthur Diniz da Luz <arthurdiluz@outlook.com>
2023-05-20 00:15:42 -0300

    feat: add repository query functions

--- src/modules/student/repositories/student.repository.ts
+++ src/modules/student/repositories/student.repository.ts
@@ -1,7 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/common/prisma/prisma.service';
 
 @Injectable()
 export class StudentRepository {
   constructor(private readonly prismaService: PrismaService) {}
+
+  public create(args: Prisma.StudentCreateArgs) {
+    return this.prismaService.student.create(args);
+  }
+
+  public find(args: Prisma.StudentFindManyArgs) {
+    return this.prismaService.student.findMany(args);
+  }
+
+  public findById(args: Prisma.StudentFindUniqueArgs) {
+    return this.prismaService.student.findUnique(args);
+  }
+
+  public update(args: Prisma.StudentUpdateArgs) {
+    return this.prismaService.student.update(args);
+  }
+
+  public delete(args: Prisma.StudentDeleteArgs) {
+    return this.prismaService.student.delete(args);
+  }
 }
