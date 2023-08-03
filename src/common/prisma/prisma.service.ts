91d95e89be9139c9f7ae85c0f485d4e2cc14412b
Arthur Diniz da Luz <arthurdiluz@outlook.com>
2023-08-03 00:38:22 -0300

    fix: prisma onModuleInit issue

--- src/common/prisma/prisma.service.ts
+++ src/common/prisma/prisma.service.ts
@@ -1,16 +1,9 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { NestApplication } from '@nestjs/core';
 import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
-  }
-
-  enableShutdownHooks(app: NestApplication) {
-    this.$on('beforeExit', async () => {
-      await app.close();
-    });
   }
 }
