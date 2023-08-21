1ce146897ebbf6966316b11b4d9a9a7e305a979f
Arthur Diniz da Luz <arthurdiluz@outlook.com>
2023-08-21 10:44:54 -0300

    Update student-lesson.controller.ts

--- src/modules/student-lesson/controllers/student-lesson.controller.ts
+++ src/modules/student-lesson/controllers/student-lesson.controller.ts
@@ -8,10 +8,10 @@ import {
   UseGuards,
 } from '@nestjs/common';
 import { StudentLessonService } from '../services/student-lesson.service';
-import { JwtAccessTokenGuard } from 'src/common/decorators/guards/jwt/jwt-access-token.guard';
 import { StudentService } from 'src/modules/student/services/student.service';
 import { ProfessorLessonService } from 'src/modules/professor-lesson/services/professor-lesson.service';
 import { CreateStudentLessonDto } from '../dtos/create-student-lesson.dto';
+import { JwtAccessTokenGuard } from 'src/common/decorators/guards/jwt/jwt-access-token.guard';
 
 @Controller('student')
 export class StudentLessonController {
@@ -20,16 +20,6 @@ export class StudentLessonController {
     private readonly studentService: StudentService,
     private readonly lessonService: ProfessorLessonService,
   ) {}
-  /*
-    Ao entrar em uma aula, deveremos obter todas os exercisos da aula e as alternativas
-    Ao final da aula, deve-se criar um registro em LessonLevelDone.
-    Caso o aluno tenha passado na aula:
-      - atualizar o registro de StudentOnLesson
-      - atualizar dados do student com o que ganho da aula
-      - relacionar medalha ao aluno 
-  */
-  // POST -> enviar resposta (alternativeId); validar se é a resposta correta; criar novo LessonLevelExerciseDone
-  // GET -> listar alternativas
 
   @UseGuards(JwtAccessTokenGuard)
   @Post(':id/lesson/:lessonId')
@@ -47,7 +37,7 @@ export class StudentLessonController {
 
       if (!(await this.lessonService.findById(lessonId))) {
         throw new BadRequestException(
-          `Student with ID #${lessonId} does not exists`,
+          `Lesson with ID #${lessonId} does not exists`,
         );
       }
 
