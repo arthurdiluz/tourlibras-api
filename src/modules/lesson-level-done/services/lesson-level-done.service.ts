c19e981ddddb3f596bb25f9c042282cfb31f63c5
Arthur Diniz da Luz <arthurdiluz@outlook.com>
2023-08-22 10:27:44 -0300

    Update lesson-level-done.service.ts

--- src/modules/lesson-level-done/services/lesson-level-done.service.ts
+++ src/modules/lesson-level-done/services/lesson-level-done.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { LessonLevelDoneRepository } from '../repositories/lesson-level-done.repository';
+import { StudentLessonService } from 'src/modules/student-lesson/services/student-lesson.service';
+import { LessonLevelService } from 'src/modules/lesson-level/services/lesson-level.service';
 
 @Injectable()
 export class LessonLevelDoneService {
   constructor(
     private readonly lessonLevelDoneRepository: LessonLevelDoneRepository,
+    private readonly studentLessonService: StudentLessonService,
+    private readonly lessonLevelService: LessonLevelService,
   ) {}
 }
