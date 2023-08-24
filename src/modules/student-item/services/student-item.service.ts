559698ffca6250e460fc0e59c6d479740298dfed
Arthur Diniz da Luz <arthurdiluz@outlook.com>
2023-08-24 20:57:53 -0300

    feat: add buy item

--- src/modules/student-item/services/student-item.service.ts
+++ src/modules/student-item/services/student-item.service.ts
@@ -1,7 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { StudentItemRepository } from '../repositories/student-item.repository';
+import { StudentService } from 'src/modules/student/services/student.service';
+import { ProfessorItemService } from 'src/modules/professor-item/services/professor-item.service';
 
 @Injectable()
 export class StudentItemService {
-  constructor(private readonly studentItemRepository: StudentItemRepository) {}
+  constructor(
+    private readonly studentItemRepository: StudentItemRepository,
+    private readonly studentService: StudentService,
+    private readonly itemService: ProfessorItemService,
+  ) {}
+
+  async linkItemToStudent(studentId: number, itemId: number) {
+    const [{ money }, { price }] = await Promise.all([
+      await this.studentService.findById(studentId),
+      await this.itemService.findById(itemId),
+    ]);
+
+    if (price > money) {
+      throw new ForbiddenException(
+        `Insufficient funds. Earn ${price - money} and try again`,
+      );
+    }
+
+    await this.studentService.update(studentId, { money: money - price });
+
+    return this.studentItemRepository.create({
+      data: {
+        Student: { connect: { id: studentId } },
+        Item: { connect: { id: itemId } },
+      },
+      include: { Student: { include: { Items: true } }, Item: true },
+    });
+  }
 }
--- src/modules/student-item/student-item.module.ts
+++ src/modules/student-item/student-item.module.ts
@@ -3,9 +3,10 @@ import { StudentModule } from '../student/student.module';
 import { StudentItemController } from './controllers/student-item.controller';
 import { StudentItemService } from './services/student-item.service';
 import { StudentItemRepository } from './repositories/student-item.repository';
+import { ProfessorItemModule } from '../professor-item/professor-item.module';
 
 @Module({
-  imports: [StudentModule, StudentItemModule],
+  imports: [StudentModule, ProfessorItemModule],
   controllers: [StudentItemController],
   providers: [StudentItemService, StudentItemRepository],
   exports: [StudentItemService],
