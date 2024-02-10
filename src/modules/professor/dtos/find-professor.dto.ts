ef92cf61b4ab58385a7d72b1e320dca6e0d11bf0
Arthur Diniz da Luz <arthurdiluz@outlook.com>
2024-02-09 23:36:29 -0300

    fix: find professor bugs

--- src/modules/professor/dtos/find-professor.dto.ts
+++ src/modules/professor/dtos/find-professor.dto.ts
@@ -1,13 +1,20 @@
-import { IsEnum, IsOptional } from 'class-validator';
+import { IsEnum, IsOptional, IsString } from 'class-validator';
 import { Transform } from 'class-transformer';
 import { FindUserDto } from 'src/modules/user/dtos/find-user.dto';
 import { GRAMMAR } from '@prisma/client';
 
 export class FindProfessorDto extends FindUserDto {
+  @Transform(({ value }) => String(value).trim())
+  @IsString()
+  @IsOptional()
+  search?: string;
+
   @Transform(({ value }) => String(value).toUpperCase())
   @IsEnum(GRAMMAR)
   @IsOptional()
   grammar?: GRAMMAR;
 
+  @IsString()
+  @IsOptional()
   sortBy?: 'Students' | 'Lessons';
 }
